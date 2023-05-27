9b36624ec03cacb48c14c317b2aca01dbebc3a49
esteve2002 <106371567+esteve2002@users.noreply.github.com>
2023-05-27 19:43:59 +0200

    Add files via upload

---
+++ Anem de ruta/html/guarda_rutes.php
@@ -0,0 +1,35 @@
+<?php
+
+
+// Conectarse a la base de datos
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'anemderuta';
+$conn = new mysqli($servername, $username, $password, $dbname);
+  $nombre = $_POST['nombreRuta'];
+  $dificultad = $_POST['dificultad'];
+  $descripcio = $_POST['descripcio'];
+  $latitud_inicial = $_POST['latitud_inicial'];
+  $latitud_final = $_POST['latitud_final'];
+  $longitud_inicial = $_POST['longitud_inicial'];
+  $longitud_final = $_POST['longitud_final'];
+  $tipus = $_POST['tipus'];
+  $sql = "INSERT INTO rutas (latitud_inicial, latitud_final, longitud_inicial, longitud_final, nombre, dificultad, descripcio, tipus) VALUES ( '$latitud_inicial', '$latitud_final', '$longitud_inicial', '$longitud_final', '$nombre', '$dificultad','$descripcio', '$tipus')";
+  $resultado = $conn->query($sql);
+  echo 'bien hecho';
+  
+
+if (!$resultado) {
+  echo "Error en la consulta: " . $conn->error;
+}
+
+$conn->close();
+
+
+
+
+
+
+
+?>
