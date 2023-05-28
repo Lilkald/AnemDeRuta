e3a50bb66558d9f1133902df1273e5e7f4ecbf32
EsteRam17 <84026738+Lilkald@users.noreply.github.com>
2023-05-28 11:54:42 +0200

    Canvis disseny Crearrutes

--- Anem de ruta/html/crear_rutes.php
+++ Anem de ruta/html/crear_rutes.php
@@ -2,7 +2,6 @@
 include("conexio/conexio.php");
     session_start();
 	if (!isset($_SESSION['username'])) {
-        // No hay sesión activa, redirigir a la página de inicio de sesión
         header("Location: login.php");
         exit();
     }
@@ -12,10 +11,7 @@ include("conexio/conexio.php");
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
-
   <style>
       @import url('https://fonts.googleapis.com/css2?family=Jaldi&display=swap');
 
@@ -23,37 +19,7 @@ include("conexio/conexio.php");
       font-family: 'Jaldi';
     }
 
-    #form-ruta{
-      border:2px solid black;
-      margin: 0 auto;
-      text-align: center;
-      width: 500px;
-      height: 100%;
-      justify-content: center;
-      align-items: center;
-      padding-top:100px;
-      padding: 15px;
-    }
-  .rating {
-    display: inline-block;
-  }
-
-  .rating input {
-    display: none;
-  }
-
-  .rating label {
-    color: #888;
-    font-size: 25px;
-    padding: 5px;
-    float: right;
-  }
-
-  .rating label:hover,
-  .rating label:hover ~ label,
-  .rating input:checked ~ label {
-    color: #ffca08;
-  }
+    
 
 
 </style>
@@ -61,12 +27,13 @@ include("conexio/conexio.php");
   
 <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/leaflet-src.min.js" integrity="sha512-nRcCZolw4mA+OKrTG/baePuVcG7PHhjFhl6pxkLYl2T+3pKEf+vXw0uR1/X3YtlPLXoWiaLjBnuUzpDuB1yXNg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>      <link rel="stylesheet" href="../leaflet/leaflet.css" />
       <script src="../leaflet/leaflet.js"></script>
-
+      <link rel="shortcut icon" href="../images/logo.png">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.css" />
       <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.js"></script>
       <link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
       <script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
+      
 </head>
 
 <body>
@@ -105,75 +72,74 @@ include("conexio/conexio.php");
             </ul>
         </nav>
     </header>
-  <br>
-  <form id="form-ruta" method="POST" onsubmit="return false;">
-  <div class="pasos" style="text-align: center;">
-    <h2>Pas 1:</h2><br>
-    <p>Introdueix les dades corresponents, amic!</p>
-  </div>
-        <label>Nom de la ruta:</label><br><br>
-        <input type="text" id="nombreRuta" name="nombreRuta" placeholder="Nombre de la ruta" required>
-        <br><br>
-        <label>Descripcio de la ruta:</label><br><br>
-        <textarea type="text" id="descripcio" name="descripcio" required></textarea>
-        <br>
-        <label>Dificultat de la ruta:</label><br><br>
-        <div class="rating">
-          <input type="radio" id="dificultad1" name="dificultad" value="5">
-          <label for="dificultad1">5</label>
-          <input type="radio" id="dificultad2" name="dificultad" value="4">
-          <label for="dificultad2">4</label>
-          <input type="radio" id="dificultad3" name="dificultad" value="3">
-          <label for="dificultad3">3</label>
-          <input type="radio" id="dificultad4" name="dificultad" value="2">
-          <label for="dificultad4">2</label>
-          <input type="radio" id="dificultad5" name="dificultad" value="1">
-          <label for="dificultad5">1</label>
-        </div>
-        <br>
-        <br>
-        <label>Tipus de ruta:</label><br><br>
-        <select name="tipo_ruta" id="tipo_ruta" required>
-          <option value="bici">Bicicleta</option>
-          <option value="coche">Coche</option>
-          <option value="moto">Moto</option>
-          <option value="caminar">Caminar</option>
-        </select>
-        <br><br>
-
-        <br>
-  </form>
-  <br>
-  <div class="pasos" style="text-align: center;">
-    <h2>Pas 2:</h2><br>
-    <p>Selecciona ara el recorregut de la teva nova ruta!</p>
-  </div>
-  
-  <div id="mapid" style="height: 600px; width: 1300px; margin: 0 auto; border: 5px solid black;"></div>
-  <br>
-  <input class="tornar" type="submit" id="guardar-ruta" name="guardar-ruta">
-  <br>
+  <main class="allRutes">
+      <div class="containerRutes">
+          <div class="dadesRuta">
+            <form id="form-ruta" method="POST" onsubmit="return false;">
+              <div class="pasos" style="text-align: center;">
+                <h2>Pas 1:</h2>
+              </div>
+                  <label>Nom de la ruta:</label>
+                  <input type="text" id="nombreRuta" name="nombreRuta" required>
+                  <label>Descripció:</label>
+                  <textarea type="text" id="descripcio" name="descripcio" required></textarea>
+                  <label>Dificultat de la ruta:</label>
+                  <div class="rating">
+                      <input type="radio" id="dificultad1" name="dificultad" value="5">
+                      <label for="dificultad1">5</label>
+                      <input type="radio" id="dificultad2" name="dificultad" value="4">
+                      <label for="dificultad2">4</label>
+                      <input type="radio" id="dificultad3" name="dificultad" value="3">
+                      <label for="dificultad3">3</label>
+                      <input type="radio" id="dificultad4" name="dificultad" value="2">
+                      <label for="dificultad4">2</label>
+                      <input type="radio" id="dificultad5" name="dificultad" value="1">
+                      <label for="dificultad5">1</label>
+                  </div>
+                  <label>Tipus de ruta:</label>
+                  <select name="tipo_ruta" id="tipo_ruta" required>
+                    <option value="bici">Bicicleta</option>
+                    <option value="coche">Coche</option>
+                    <option value="moto">Moto</option>
+                    <option value="caminar">Caminar</option>
+                  </select>
+            </form>
+          </div>
+          <br>
+          <div class="pasos" style="text-align: center;">
+            <h2>Pas 2:</h2><br>
+            <div id="mapid" style="height: 80%; width: 100%; margin: 0 auto; border: 5px solid black;"></div>
+            <br>
+          </div>
+      </div>
+      <div class="botoSeguir botoEditar botoEnviar llarguet">
+        <form class="editar" method="POST" action="editor.php" enctype="multipart/form-data"> 
+          <input class="buttons" type="submit" id="guardar-ruta" name="guardar-ruta">
+        </form>
+      </div> 
+  </main>
+  <!--Footer-->
   <footer>
-    <div class="footer">
-        <div class="redes sect">
-            <p>SIGUENOS EN:</p>
-            <div class="logo">
-                <a href="https://www.instagram.com%22%3E/<i class='bx bxl-instagram'></i></a>
-                <a href="https://www.twitter.com%22%3E/<i class='bx bxl-twitter'></i></a>
-                <a href="https://www.facebook.com%22%3E/<i class='bx bxl-facebook-circle'></i></a>
+        <div class="footer">
+            <div class="redes sect">
+                <p>SIGUENOS EN:</p>
+                <div class="logo">
+                    <a href="https://www.instagram.com"><i class='bx bxl-instagram'></i></a>
+                    <a href="https://www.twitter.com"><i class='bx bxl-twitter'></i></a>
+                    <a href="https://www.facebook.com"><i class='bx bxl-facebook-circle'></i></a>
+                </div>
+            </div>
+            <div class="allright sect">
+                <p>&copy; NosVamosDeRuta. All rights reserved.</p>
+                <a href="">Condiciones de uso</a> | <a href="">Privacidad</a> | <a href="">Política de cookies</a>
+            </div>
+            <div class="politica sect">
+                <a href="">Política de privacidad</a>
+                <a href="">Sobre nosotros</a>
+                <a href="">Nuestras Tarifas</a>
             </div>
         </div>
-        <div class="allright sect">
-            <p>&copy; NosVamosDeRuta. All rights reserved.</p>
-            <a href="">Condiciones de uso</a> | <a href="">Privacidad</a> | <a href="">Política de cookies</a>
-        </div>
-        <div class="politica sect">
-            <a href="">Política de privacidad</a>
-            <a href="">Sobre nosotros</a>
-            <a href="">Nuestras Tarifas</a>
-        </div>
-    </div>
-</footer>
+    </footer>
     </body>
 
   <script>
@@ -199,37 +165,26 @@ function onGeolocationSuccess(position) {
       var lat = position.coords.latitude;
       var lon = position.coords.longitude;
 
-      // Create a marker for the current location
       var marker = L.marker([lat, lon]).addTo(map);
 
-      // Set the map view to the current location
       map.setView([lat, lon], 13);
     }
 
-    // Function to handle geolocation error
+
     function onGeolocationError(error) {
       console.error(error.message);
     }
 
     if ('geolocation' in navigator) {
-      // Get the current position
       navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationError);
     } else {
       console.error('Geolocation is not supported by your browser.');
     }
 
-
-
-
-
-
-// Agregar control de geocodificación inversa
   map.on('markgeocode', function (e) {
-    // Obtener información de ubicación del resultado de geocodificación
     const location = e.geocode.center;
     const address = e.geocode.name;
 
-    // Imprimir información de ubicación en la consola o hacer lo que necesites con ella
     console.log('Coordenadas:', location);
     console.log('Dirección:', address);
 
@@ -241,7 +196,6 @@ function onGeolocationSuccess(position) {
       map.setView(result.center, 13);
     });
 
-// Agregar control de geocodificación inversa al mapa
 geocoderControl.addTo(map);
 
 
@@ -267,7 +221,6 @@ var drawControl = new L.Control.Draw({
   }
 });
 map.addControl(drawControl);
-// Almacenar las rutas en un arreglo
 var coordenadas = [];
 
 
@@ -280,7 +233,6 @@ function layerType(layer) {
 }
 
 
-// Escuchar el evento de dibujar un polyline y agregarlo al arreglo de rutas
 map.on('draw:created', function (event) {
   var layer = event.layer;
   var type = layerType(layer);
@@ -300,7 +252,6 @@ map.on('draw:created', function (event) {
     var endLat = endLatLng.lat;
     var endLng = endLatLng.lng;
     
-    // Aquí puedes hacer algo con las coordenadas de la polilínea (inicio y fin)
     console.log('Punto de inicio - Latitud:', startLat, 'Longitud:', startLng);
     localStorage.setItem('latitud_inicial', startLat);
     localStorage.setItem('latitud_final', endLat);
@@ -330,7 +281,7 @@ function obtenerDificultadSeleccionada() {
     return valorSeleccionado;
   }
   var usuari = "<?php echo $usuari; ?>";
-    console.log(usuari);
+
 
 $('#guardar-ruta').click(function() {
     var latitud_inicial = localStorage.getItem('latitud_inicial');
